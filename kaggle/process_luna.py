ae011326ee50132af22218990d53724f5296ae44
oymeir <oymeir@gmail.com>
2017-03-28 15:00:11 +0300

    Ready to train on LUNA

--- kaggle/process_luna.py
+++ kaggle/process_luna.py
@@ -1,18 +1,20 @@
 import csv
-import glob     # for finding file within subdirs
+import glob     # for finding file within subdirs, python >= 3.5
+import fnmatch  # for finding file within subdirs, python < 3.5
 import random
 import os.path
 import SimpleITK as sitk    # for reading LUNA2016 mhd files
 import numpy as np
 import h5py
 import scipy.ndimage
+import kaggle.train as kgtrain     # for normalize_hu, RAND_SEED
 
 PATH_CANDIDATES_CSV = "/razberry/datasets/luna16/candidates.csv"
 PATH_DATA = "/razberry/datasets/luna16"
 RATIO_NEG_TO_POS = 2
 PATH_OUTPUT = "/razberry/datasets/kaggle-dsb2017/luna2016_processed"
 SZ_CUBE = 54
-
+RAND_SEED = kgtrain.RAND_SEED
 
 def get_candidates():
 
@@ -40,8 +42,11 @@ def get_candidates():
 
 def load_scan(scan_id):
     
-    # find the file
-    file = glob.glob(PATH_DATA + "/**/" + scan_id + ".mhd", recursive=True)
+    #file = glob.glob(PATH_DATA + "/**/" + scan_id + ".mhd", recursive=True)
+    file = []
+    for root, dirnames, filenames in os.walk(PATH_DATA):
+        for filename in fnmatch.filter(filenames, scan_id + ".mhd"):
+            file.append(os.path.join(root, filename))
 
     # From: https://www.kaggle.com/arnavkj95/data-science-bowl-2017/candidate-generation-and-luna16-preprocessing
     # adapted from `load_itk`
@@ -69,7 +74,17 @@ def load_scan(scan_id):
 
 
 def gen_candidates(ct_scan, world_2_voxel, candidates):
-    """Generates 3D crops around specified coordinates."""
+    """Generates 3D crops around specified coordinates.
+    
+    Args:
+        ct_scan: 3D numpy array with CT scan's voxels
+        world_2_voxel: a function handle for mapping world coordinates to voxels
+        candidates: candidate detections read from CSV (id, x, y, z, mm, label)
+
+    Saves the candidate crops to 2 HDF5 files: "1.h5", "0.h5" corresponding to
+    the data's label. The keys in each file correspond to scan ID, and the
+    value for each key is a 4D numpy array of detections (x, y, z, N_DETECT)
+    """
 
     n = np.floor(SZ_CUBE / 2).astype(np.int16)
     crops = np.zeros((SZ_CUBE, SZ_CUBE, SZ_CUBE, len(candidates)),
@@ -105,6 +120,53 @@ def gen_candidates(ct_scan, world_2_voxel, candidates):
 
     save_crop_to_file("0.h5", crops[:, :, :, np.logical_not(is_pos)])
     save_crop_to_file("1.h5", crops[:, :, :, is_pos])
+
+def luna_generator(d0, d1, batch_size=8, ids=None):
+    """Generates batches for training and validation from LUNA2016 set.
+
+    Args:
+        d0, d1: dictionaries for non-class/class resp. key is patient id
+        batch_size: required batch size (default 8)
+        ids: use these scan IDs (for train/val split)
+
+    Returns:
+        (data, labels): batch with data and labels
+    """
+
+    np.random.seed(RAND_SEED)
+
+    # construct the samples tuples (patient_id, label, idx)
+    def load_samples(d, label):
+        with h5py.File(os.path.join(PATH_OUTPUT, "{}.h5".format(label))) as f:
+            return [(entry[0], label, i)
+                    for i in range(0, f[entry[0]].shape[-1])
+                    for entry in d if entry[0] in ids]
+    samples = load_samples(d0, 0) + load_samples(d1, 1)
+
+    inds_shuffled = np.random.permutation(len(samples))
+    normalize_hu = kgtrain.normalize_hu
+    
+    with h5py.File(PATH_PROCESSED_H5, "r") as fh5:
+        while True:
+
+            if len(inds_shuffled) < batch_size:
+                inds_shuffled = np.random.permutation(len(samples))
+            
+            data = np.zeros((batch_size, ) + kgtrain.INPUT_SHAPE + (9,),
+                            dtype = np.float32)
+            for i in inds_shuffled[0:batch_size]:
+                cube = fh5.get(samples[i][0]).value[:, :, :, samples[i][2]]
+                crop_inds = np.random.randint(0, SZ_CUBE -
+                                              kgtrain.INPUT_SHAPE[0], 3)
+                cube = cube[crop_inds[0]:kgtrain.INPUT_SHAPE[0],
+                            crop_inds[1]:kgtrain.INPUT_SHAPE[0],
+                            crop_inds[2]:kgtrain.INPUT_SHAPE[0]]
+                data[:, :, :, i] = kgtrain.normalize_hu(
+                    kgtrain.slice_cube(cube))
+            labels = [samples[i][1] for i in inds_shuffled[0:batch_size]]
+            inds_shuffled = np.delete(inds_shuffled, range(0, batch_size))
+
+            yield np.expand_dims(data, axis=5), labels
 
 ### Iterate candidates and extract samples
 d0, d1 = get_candidates()
--- kaggle/train.py
+++ kaggle/train.py
@@ -13,8 +13,12 @@ import random
 import os
 import skimage.io
 
+import kaggle.process_luna as kgluna    # for luna_generator, get_candidates
+
 PATH_LABELS_CSV = "C:/DATA/projects/kaggle/data/stage1_labels.csv"
-PATH_PROCESSED_H5 = "kaggle_train_processed.h5"
+PATH_DETECTIONS_CSV = "/path/to.csv"
+PATH_PROCESSED_CT = "/razberry/datasets/kaggle-dsb2017/stage1_processed"
+PATH_PROCESSED_H5 = "/razberry/datasets/kaggle-dsb2017/stage1_rois.h5"
 PATH_UNET = "C:\\DATA\\scratch\\models\\unet\\unet.hdf5"
 RAND_SEED = 13
 INPUT_SHAPE = (48, 48,)
@@ -195,24 +199,37 @@ def split_train_val(ratio_train=0.7):
 
     return train, val
 
+def process_detections():
 
-# TODO: cross-validation?
-# TODO: augmentation?
+    with open(PATH_DETECTIONS_CSV, "r") as f:
+        reader = csv.reader(f)
+        detections = list(reader)
+    
+    with h5py.File(PATH_PROCESSED_H5, 'w') as fh5:
+        # iterate detections and create the ROI database
+        for patient in detections:
+            pass
 
+#train, val = split_train_val()
+#train = random.sample(train, 30)
+#val = random.sample(val, 10)
+#gen_simulated_data(train + val)
 
-train, val = split_train_val()
-train = random.sample(train, 30)
-val = random.sample(val, 10)
-gen_simulated_data(train + val)
+# load luna dataset, split to train and validation
+d0, d1 = kgluna.get_candidates()
+ids_train = [item[0] for item in
+             np.sample(list(d1.keys()), int(np.round(0.7 * len(d1))))]
+ids_val = set(d1.keys).difference(set(ids_train))
 
 model = get_unet(PATH_UNET)
 model_checkpoint = ModelCheckpoint("unet_trained.hdf5", monitor="loss",
                                    save_best_only=True)
-history = model.fit_generator(kaggle_generator(train, 2),
-                              steps_per_epoch=3,
-                              epochs=5,
-                              validation_data=kaggle_generator(val, 2),
-                              validation_steps=1,
+history = model.fit_generator(kgluna.luna_generator(d0, d1, 8, ids_train),
+                              steps_per_epoch=25,
+                              epochs=3,
+                              validation_data=
+                              kgluna.luna_generator(d0, d1, 8, ids_val),
+                              validation_steps=5,
                               callbacks=[model_checkpoint],
                               verbose=1)
 
@@ -241,21 +258,21 @@ history = model.fit_generator(kaggle_generator(train, 2),
 #          shuffle=True, callbacks=[model_checkpoint])
 
 
-## TODO: print also the starting point
-## summarize history for accuracy
-#plt.plot(history.history["acc"])
-#plt.plot(history.history["val_acc"])
-#plt.title("model accuracy")
-#plt.ylabel("accuracy")
-#plt.xlabel("epoch")
-#plt.legend(["train", "test"], loc="upper left")
-#plt.savefig("accuracy.png")
-## summarize history for loss
-#plt.cla()
-#plt.plot(history.history["loss"])
-#plt.plot(history.history["val_loss"])
-#plt.title("model loss")
-#plt.ylabel("loss")
-#plt.xlabel("epoch")
-#plt.legend(["train", "test"], loc="upper left")
-#plt.savefig("loss.png")
+# TODO: print also the starting point
+# summarize history for accuracy
+plt.plot(history.history["acc"])
+plt.plot(history.history["val_acc"])
+plt.title("model accuracy")
+plt.ylabel("accuracy")
+plt.xlabel("epoch")
+plt.legend(["train", "test"], loc="upper left")
+plt.savefig("accuracy.png")
+# summarize history for loss
+plt.cla()
+plt.plot(history.history["loss"])
+plt.plot(history.history["val_loss"])
+plt.title("model loss")
+plt.ylabel("loss")
+plt.xlabel("epoch")
+plt.legend(["train", "test"], loc="upper left")
+plt.savefig("loss.png")
