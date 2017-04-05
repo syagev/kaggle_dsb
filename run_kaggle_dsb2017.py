372991a367f9bc38351240bec9a4cd1e5dffda7f
oymeir <oymeir@gmail.com>
2017-04-05 09:56:55 +0300

    Setup a script for the Kaggle submission.

---
+++ run_kaggle_dsb2017.py
@@ -0,0 +1,76 @@
+"""Process, detect, train and predict"""
+
+import kaggle.classifier
+import kaggle.util
+import subprocess
+import csv
+import os
+import keras.optimizers
+
+# paths to raw data
+PATH_TRAIN_DATA = "/path/to/training_data"
+PATH_TEST_DATA = "/path/to/test/data"
+PATH_TRAIN_LABELS = r"C:\DATA\projects\deep-lung\data\stage1_labels.csv"
+
+# paths to processed data
+PATH_TRAIN_PROCESSED = "/path/to/train_proc"
+PATH_TEST_PROCESSED = "/path/to/test_proc"
+
+# other paths
+PATH_DATASETS = "/razberry/datasets/kaggle-dsb2017"
+PATH_WORKSPACE = "/razberry/workspace"
+
+# parameters
+N_CROSS_VAL = 10
+
+# session header
+version = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'])
+version = version.strip().decode("ascii")
+path_session = os.path.join(PATH_WORKSPACE, "dsb2017_{}".format(version))
+os.mkdir(path_session)
+
+# TODO: process (both train and test)
+
+
+
+# TODO: detection (both train and test)
+train_csv = os.path.join(PATH_DATASETS, "detections_train.csv")
+test_csv = os.path.join(PATH_DATASETS, "detections_test.csv")
+
+# extract crops around detections (both train and test)
+kaggle.util.extract_detections(PATH_TRAIN_DATA, train_csv,
+                               os.path.join(PATH_DATASETS,
+                                            "detections_train.hdf5"))
+kaggle.util.extract_detections(PATH_TEST_DATA, test_csv,
+                               os.path.join(PATH_DATASETS,
+                                            "detections_test.hdf5"))
+
+# train an ensemble of classifiers
+hyper_param = {
+    "epochs" : 10,
+    "batch_sz": [8],
+    "batch_norm": [True, False],
+    "optimizers": [keras.optimizers.Adam(1e-4)],
+    "lr_scheduler_param": [(1e-4, 5, 10),
+                            (1e-4, 10, 20)]
+    }
+
+models =[]
+for i in range(0, N_CROSS_VAL):
+
+    # split to training and validation sets
+    train, val = kaggle.classifier.split_train_val(PATH_TRAIN_LABELS,
+                                                   int(version, 16) + i)
+
+    models.append(kaggle.classifier.train_ensemble(
+        train,
+        val,
+        os.path.join(PATH_DATASETS, "detections_train.hdf5"),
+        path_session,
+        hyper_param))
+
+
+# TODO: predict on test dir (stage-1 heldout and stage-2)
+# TODO: evaluate all models, consider different mixtures:
+#   average, median, voting, different subsets, etc
+
