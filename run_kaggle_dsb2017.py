60ba69e186f013b095db2a1f3bf86688f8e60deb
oymeir <oymeir@gmail.com>
2017-04-06 11:50:31 +0000

    fix version inconsistency in pickle

--- run_kaggle_dsb2017.py
+++ run_kaggle_dsb2017.py
@@ -19,7 +19,7 @@ PATH_TRAIN_LABELS = "/razberry/datasets/kaggle-dsb2017/stage1_labels.csv"
 
 # paths to processed data
 PATH_TRAIN_PROCESSED = "/path/to/train_proc"
-PATH_TEST_PROCESSED = "/path/to/test_proc"
+PATH_TEST_PROCESSED = "/razberry/datasets/kaggle-dsb2017/stage1_processed_val"
 
 # other paths
 PATH_DATASETS = "/razberry/datasets/kaggle-dsb2017"
@@ -67,31 +67,32 @@ hyper_param = {
 
 models =[]
 session_id = os.path.basename(path_session)
-for i in range(0, N_CROSS_VAL):
+# for i in range(0, N_CROSS_VAL):
 
-    print("*** Training and cross validation {}/{}".format(i + 1, N_CROSS_VAL))
+    # print("*** Training and cross validation {}/{}".format(i + 1, N_CROSS_VAL))
 
-    # split to training and validation sets
-    train, val = kaggle.classifier.split_train_val(PATH_TRAIN_LABELS,
-                                                   seed=int(version, 16) + i)
+    # # split to training and validation sets
+    # train, val = kaggle.classifier.split_train_val(PATH_TRAIN_LABELS,
+                                                   # seed=int(version, 16) + i)
 
-    path_session_i = os.path.join(path_session, "{}_".format(i) + session_id)
-    if not os.path.exists(path_session_i):
-        os.mkdir(path_session_i)
+    # path_session_i = os.path.join(path_session, "{}_".format(i) + session_id)
+    # if not os.path.exists(path_session_i):
+        # os.mkdir(path_session_i)
 
-    models.append(kaggle.classifier.train_ensemble(
-        train,
-        val,
-        os.path.join(PATH_DATASETS, "stage1_detections_mock.hdf5"),
-        path_session_i,
-        hyper_param))
+    # models.append(kaggle.classifier.train_ensemble(
+        # train,
+        # val,
+        # os.path.join(PATH_DATASETS, "stage1_detections_mock.hdf5"),
+        # path_session_i,
+        # hyper_param))
 
 
 # predict on test dir (stage-1 holdout and stage-2)
+models = kaggle.classifier.load_ensemble("/razberry/workspace/dsb2017.788268e")
 test_ids = [os.path.splitext(id)[0] for id in os.listdir(PATH_TEST_PROCESSED)]
 kaggle.classifier.predict_ensemble(
     models,
-    os.path.join(PATH_DATASETS, "stage1_detections_mock.hdf5"),
+    os.path.join(PATH_DATASETS, "stage1_detections.hdf5"),
     test_ids,
     os.path.join(path_session))
 
