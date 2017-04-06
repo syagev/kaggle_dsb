fb044d7dbf57c633a2c99ae6b7d1548fcc7351b2
oymeir <oymeir@gmail.com>
2017-04-06 08:34:33 +0000

    Fix extract_detections to account for offset

--- kaggle/util.py
+++ kaggle/util.py
@@ -29,11 +29,16 @@ def extract_detections(path_data, path_csv, path_output):
         with h5py.File(path_output, "w") as f_h5:
             for id, coords in detections.items():
 
-                # load CT scan (ct_scan is [z, x, y])
+            # load CT scan (ct_scan is [z, x, y])
                 try:
 
+                    # detections.csv seriesuid string is missing chars
+                    file_id = [f for f in os.listdir(path_data) if id in f]
+                    assert(len(file_id) == 1)
+                    id_ = os.path.splitext(file_id[0])[0]
+                    
                     ct_scan = np.load(os.path.join(path_data, 
-                                                   "{}.npy".format(id)))
+                                                   "{}.npy".format(id_)))
                     crops = np.zeros((CUBE_SZ, CUBE_SZ, CUBE_SZ, len(coords)))
 
                     # pad ct_scan and crop
@@ -44,6 +49,12 @@ def extract_detections(path_data, path_csv, path_output):
                     ct_scan_shape = ct_scan.shape
                     ct_scan = np.pad(ct_scan, CUBE_SZ, "constant")
                     for i, xyz in enumerate(coords):
+                        
+                        # fix offset in x,y (detections in 512x512 window)
+                        xyz = list(xyz)
+                        xyz[0] = xyz[0] - int((512-ct_scan_shape[1])/2)
+                        xyz[1] = xyz[1] - int((512-ct_scan_shape[2])/2)
+                    
                         try:
                             crops[:, :, :, i] = ct_scan[
                                 xyz[2] + CUBE_SZ : xyz[2] + 2 * CUBE_SZ,
@@ -54,10 +65,10 @@ def extract_detections(path_data, path_csv, path_output):
                         except ValueError:
                             print("*** ERROR in {}".format(i_counter))
                             log_file.write("Error in {}, shape: {}, xyz: {}\n" \
-                                .format(id, ct_scan_shape, xyz))
+                                .format(id_, ct_scan_shape, xyz))
 
                     # write
-                    f_h5.create_dataset(id,
+                    f_h5.create_dataset(id_,
                                         shape=crops.shape,
                                         dtype=np.int16,
                                         data=crops)
